ae795d3a9b2811768ed95a74752ab5748448a651
Emre-Gelen <61112809+Emre-Gelen@users.noreply.github.com>
2020-04-11 13:18:42 +0300

    Initial commit

---
+++ 2Donusum/Kesir.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Donusum
+{
+    class Kesir
+    {
+        public int Pay;
+        public int Payda;
+
+        public Kesir(int p,int pd)
+        {
+            Pay = p;
+            Payda = pd;
+        }
+
+        public void Yaz()
+        {
+            Console.WriteLine("{0}/{1}", Pay, Payda);
+        }
+
+        public static explicit operator int(Kesir k)
+        {
+            //donusumun nasil yapilacagi tarif edilir
+            int sonuc = k.Pay / k.Payda;
+            return sonuc;
+
+        }
+
+        public static implicit operator Kesir(int sayi)
+        {
+            Kesir k = new Kesir(sayi, 1);
+            return k;
+        }
+
+        public static Kesir operator +(Kesir k1, Kesir k2)
+        {
+            int yp = k1.Pay * k2.Payda + k2.Pay * k1.Payda;
+            int ypd = k1.Payda * k2.Payda;
+            Kesir k = new Kesir(yp, ypd);
+            return k;
+        }
+
+        public static bool operator ==(Kesir k1, Kesir k2)
+        {
+            double sonuc1 = k1.Pay / k1.Payda;
+            double sonuc2 = k2.Pay / k2.Payda;
+            return sonuc1 == sonuc2;
+
+        }
+
+        public static bool operator !=(Kesir k1, Kesir k2)
+        {
+            return !(k1 == k2);
+        }
+
+
+
+    }
+}
---
+++ 2Donusum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Donusum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kesir k1 = new Kesir(5, 3);
+            //k1.Yaz();
+
+            int a = (int)k1;
+            Console.WriteLine(a);
+
+            Kesir k2 = 5;
+            k2.Yaz();
+
+            Kesir k3 = k1 + k2;
+            k3.Yaz();
+
+           Kesir k4 = 3 + k3;
+            k4.Yaz();
+
+            Kesir k5 = new Kesir(5, 1);
+            Kesir k6 = new Kesir(10, 2);
+            if (k5 == k6)
+                Console.WriteLine("esit");
+            else
+                Console.WriteLine("esit degil");
+
+        }
+    }
+}
